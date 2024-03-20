f8ac3e3702f5ac8cfb5fb5131224e775b602fba1
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2024-03-20 15:53:55 -0300

    fiz mais 5

--- routes/api.php
+++ routes/api.php
@@ -39,70 +39,70 @@ Route::get('calculo', function (request $request) {
 Route::get('divi', function (request $request) {
     $numeroUm = $request->input('primeiroNumero');
     $numeroDois = $request->input('segundoNumero');
-    $resultado =$numeroUm / $numeroDois;
+    $resultado = $numeroUm / $numeroDois;
     return $resultado;
 });
 
-route::get('multi', function (request $request) {
+Route::get('multi', function (request $request) {
     $numeroUm = $request->input('primeiroNumero');
     $numeroDois = $request->input('segundoNumero');
-    $resultado =$numeroUm * $numeroDois;
+    $resultado = $numeroUm * $numeroDois;
     return $resultado;
 });
 
-route::get('multiplicacao', function (request $request) {
+Route::get('multiplicacao', function (request $request) {
     $numeroUm = $request->input('primeiroNumero');
     $numeroDois = $request->input('segundoNumero');
     $numeroTres = $request->input('terceiroNumero');
     $numeroQuatro = $request->input('quartoNumero');
     $numeroCinco = $request->input('quintoNumero');
-    $resultado = ($numeroUm + $numeroDois + $numeroTres + $numeroQuatro + $numeroCinco) /5;
+    $resultado = ($numeroUm + $numeroDois + $numeroTres + $numeroQuatro + $numeroCinco) / 5;
     return $resultado;
 });
 
-route::get('receber', function (request $request) {
+Route::get('receber', function (request $request) {
     $numeroUm = $request->input('primeiroNumero');
     $numeroDois = $request->input('segundoNumero');
-    $resultado =$numeroDois / $numeroUm;
+    $resultado = $numeroDois / $numeroUm;
     return $resultado;
 });
 
-route::get('dobro', function (request $request) {
+Route::get('dobro', function (request $request) {
     $numeroUm = $request->input('primeiroNumero');
     $resultado = "O dobro do " . $numeroUm . " é igual a " . $numeroUm * 2;
     return $resultado;
 });
 
-route::get('area', function (request $request) {
+Route::get('area', function (request $request) {
     $numeroUm = $request->input('base');
-    $numeroDois = $request ->input('altura');
+    $numeroDois = $request->input('altura');
     $resultado = "a area é " . $numeroUm * $numeroDois;
     return $resultado;
 });
 
-route::get('porcentagens', function (request $request) {
+Route::get('porcentagens', function (request $request) {
     $numeroUm = $request->input('precoOriginal');
-    $numeroDois = $request ->input('porcentagem');
+    $numeroDois = $request->input('porcentagem');
     $resultado = $numeroUm / 100;
     $resultado2 = $numeroUm - ($resultado * $numeroDois);
     return $resultado2;
 });
 
-route::get('salario', function (request $request) {
+Route::get('salario', function (request $request) {
     $numeroUm = $request->input('salario');
-    $numeroDois = $request ->input('aumento');
+    $numeroDois = $request->input('aumento');
     $resultado = $numeroUm / 100;
     $resultado2 = $numeroUm . " " . $numeroDois . "% " . $resultado * $numeroDois + $numeroUm;
     return $resultado2;
 });
 
-route::get('compra', function (request $request) {
+Route::get('compra', function (request $request) {
     $numeroUm = $request->input('valorCompra');
     $resultado2 = $numeroUm / 10;
     return $resultado2;
 });
 
-route::get('recebendo', function (request $request) {
+Route::get('recebendo', function (request $request) {
     $numeroUm = 5;
     $numeroDois = $request->input('recebido');
     $porcentagem = $numeroDois / 100;
@@ -110,7 +110,7 @@ route::get('recebendo', function (request $request) {
     return $resultado2;
 });
 
-route::get('days', function (request $request) {
+Route::get('days', function (request $request) {
     $Dias = $request->input('dias');
     $horas = $Dias * 24;
     $minutos = $horas * 60;
@@ -119,20 +119,81 @@ route::get('days', function (request $request) {
     return $resultado;
 });
 
-route::get('buy', function (request $request) {
+Route::get('buy', function (request $request) {
     $compras = $request->input('compras');
-     $quantidade = $request->input('quantidade');
-     $resultado = $compras * $quantidade;
-     return $resultado;
- });
+    $quantidade = $request->input('quantidade');
+    $resultado = $compras * $quantidade;
+    return $resultado;
+});
 
-route::get('exemplo/condicao', function (request $request) {
-$idade = $request->input('idade');
-$retorno = "";
-if($idade >=18){
-    $retorno = "maior de idade ";
+Route::get('exemplo/condicao', function (request $request) {
+    $idade = $request->input('idade');
+    $retorno = "";
+    if ($idade >= 18) {
+        $retorno = "maior de idade ";
+    } else {
+        $retorno = "Menor de idade";
+    }
+    return $retorno;
+});
+
+Route::get('verifica/idade', function(Request $request) {
+    $idade = $request->input('idade');
+
+    if ($idade >= 18) {
+        return "maior de idade";
+    } else {
+        return "menor de idade";
+    }
+});
+
+
+Route::get('verificar/par' , function(Request $request){
+    $numero = $request-> input('numero');
+
+    if($numero % 2 == 0)
+    return 'par';
+else {
+    return 'impar';
 }
-else{
-    $retorno = "Menor de idade";
-}
- });
+
+
+});
+
+Route::get("receber/numero" , function(Request $request){
+    $numero = $request-> input("numero"); 
+    
+    if($numero > 10){
+        return "maior que 10";
+     } else {
+            return "menor que 10";
+        }
+    });
+
+
+    Route::get("temperatura/atual" , function(Request $request){
+        $temperatura = $request-> input ("temperatura atual");
+      
+        if($temperatura > 30){
+        return "está quente";
+    } else { 
+              return "não esta quente";
+         }
+    });
+
+
+    Route::get("determine/numero" , function(Request $request){
+        $numero = $request-> input ('numero');
+        if($numero > 0){
+        return "é positivo";
+        }
+        if($numero < 0){
+        return "é negativo";
+        }
+         else {
+    return "é zero";
+        }
+    });
+        
+
+
